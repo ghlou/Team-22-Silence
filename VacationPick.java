c7b4646c49c12f26fa75ec18d76a75f07c5b8c93
ghlou <44778806+ghlou@users.noreply.github.com>
2018-11-05 19:25:23 -0500

    Add files via upload

---
+++ VacationPick.java
@@ -0,0 +1,19 @@
+public class VacationPick {
+  public static void main(String[] args){
+    System.out.println("Enter your budget: ");
+    double budget = TextIO.getlnDouble();
+    System.out.println("Enter the season of trip: ");
+    String season = TextIO.getlnString();
+    System.out.println("Enter preferred temperature in F: ");
+    double temperature = TextIO.getlnDouble();
+    System.out.println("Enter preferred humidity in % out of 100: ");
+    double humiditiy = TextIO.getlnDouble();
+    System.out.println("Choose what type of vacation you are looking for-relaxation, adventure, or historical: ");
+    String type = TextIO.getlnString();
+    
+
+    )
+    )
+  }
+
+}
