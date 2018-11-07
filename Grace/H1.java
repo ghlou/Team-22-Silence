7614e51b78f42c697b5b72d54e0334f8908ffebd
Grace Lou <grace.h.lou@gmail.com>
2018-11-07 18:04:24 -0500

    Update Grace/H1.java

---
+++ Grace/H1.java
@@ -0,0 +1,32 @@
+public class H1 {
+     public static void main (String[] args) {
+       String [] tours={"","","","","","","","",""};
+ System.out.println("What kind of weather do you prefer? Enter warm, cool, snowy, or nevermind.");
+       String weather=TextIO.getlnString();
+ String nyc = "New York City";
+    String h = "Hawaii";
+    String r = "Rome";
+    String c = "Cancun";
+    String o = "Orlando";
+    String l = "London";
+    String m = "Miami";
+    String lv = "Las Vegas";
+    String sf = "San Francisco";
+    String la = "Los Angeles";
+    String mb = "Myrtle Beach";
+    String pr = "Puerto Rico";
+    String p = "Paris";
+
+switch (weather)
+{case "warm" :tours[0]=nyc;tours[1]= h;
+
+
+
+
+}
+
+System.out.print(tours[1]);
+
+
+
+     }}
