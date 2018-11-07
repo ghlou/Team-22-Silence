aae57cd50a88ff7418bbc435240a91490f83753a
Yuran <smax001@gmail.com>
2018-11-07 18:08:15 -0500

    Update Jiayu/H1.java

--- Jiayu/H1.java
+++ Jiayu/H1.java
@@ -7,13 +7,14 @@ public class H1 {
     String h="Hawaii";
     
 switch (weather)
-{case "warm" :tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;break;
-  case "hot": tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr;break;
-  case "cool": tours[0]=r;tours[1]=l; tours[2]=p;break;
-  case "nevermind": tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;
-   tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr; 
-   tours[0]=r;tours[1]=l; tours[2]=p;break;
-  default: "Sorry, I cannot understand."; 
+{  
+    case "warm" :tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;break;
+    case "hot": tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr;break;
+    case "cool": tours[0]=r;tours[1]=l; tours[2]=p;break;  
+    case "nevermind": tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;
+      tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr; 
+      tours[0]=r;tours[1]=l; tours[2]=p;break;
+    default: "Sorry, I cannot understand."; 
 }
    
      }}
