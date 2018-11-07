80bb72da7bb46b887c351c83995f39b76a5a1541
836929796 <44778881+836929796@users.noreply.github.com>
2018-11-07 18:27:34 -0500

    Update H1.java

--- Jiayu/H1.java
+++ Jiayu/H1.java
@@ -1,20 +1,38 @@
 public class H1 {
      public static void main (String[] args) {
-       String [] tours=new String[13]; 
- System.out.println("What kind of weather do you prefer? Enter warm, cool, snowy, or nevermind.");
+       String [] tours={"","","","","","","","","","","","",""};
+       System.out.println("Hello friend welcome to Silence Project.");
+       System.out.println("What kind of weather do you prefer? Enter warm, cool, snowy, or nevermind.");
        String weather=TextIO.getlnString();
- String nyc="New York City";      
-    String h="Hawaii";
-    
+       String nyc="New York City";
+       String h="Hawaii";
+       String r = "Rome";
+       String c = "Cancun";
+       String o = "Orlando";
+       String l = "London";
+       String m = "Miami";
+       String lv = "Las Vegas";
+       String sf = "San Francisco";
+       String la = "Los Angeles";
+       String mb = "Myrtle Beach";
+       String pr = "Puerto Rico";
+       String p = "Paris";
 switch (weather)
-{  
-    case "warm" :tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;break;
-    case "hot": tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr;break;
-    case "cool": tours[0]=r;tours[1]=l; tours[2]=p;break;  
-    case "nevermind": tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;
-      tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr; 
-      tours[0]=r;tours[1]=l; tours[2]=p;break;
-    default: "Sorry, I cannot understand."; 
+{case "warm" :tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;break;
+  case "hot": tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr;break;
+  case "cool": tours[0]=r;tours[1]=l; tours[2]=p;break;
+  case "nevermind": tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;
+   tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr;
+   tours[0]=r;tours[1]=l; tours[2]=p;break;
+  default: tours[1]="Sorry, I cannot understand.";
 }
-   
+
+for (int nn=0;nn<=12;nn++)
+{System.out.println(tours[nn]);
+
+
+}
+
+
+
      }}
