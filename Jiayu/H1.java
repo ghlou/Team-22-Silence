753dc5c2ba4e8684d1d6cbf17c1c7c9e05c97373
836929796 <44778881+836929796@users.noreply.github.com>
2018-11-20 10:39:50 -0500

    Update H1.java

--- Jiayu/H1.java
+++ Jiayu/H1.java
@@ -1,9 +1,19 @@
 public class H1 {
      public static void main (String[] args) {
+ 
+       System.out.println("Hi, Brandeis Students! Let's plan for your vocation.");
+    
+       //weather
        String [] tours={"","","","","","","","","","","","",""};
-       System.out.println("Hello friend welcome to Silence Project.");
-       System.out.println("What kind of weather do you prefer? Enter warm, cool, snowy, or nevermind.");
+       System.out.println("What kind of weather do you prefer? Enter \"warm\", \"cool\", \"snowy\", or \"nevermind\"");
        String weather=TextIO.getlnString();
+       
+       //location
+       String [] tl={"","","","","","","","","","","","",""};
+       System.out.println("How far do you prefer? Enter \"domestic \", \"International\", or \"nevermind\"");
+       String location=TextIO.getlnString();
+       
+       //declare city names
        String nyc="New York City";
        String h="Hawaii";
        String r = "Rome";
@@ -17,6 +27,8 @@ public class H1 {
        String mb = "Myrtle Beach";
        String pr = "Puerto Rico";
        String p = "Paris";
+
+       //weather
 switch (weather)
 {case "warm" :tours[0]=nyc;tours[1]=lv; tours[2]=sf;tours[3]=la;tours[4]=mb;break;
   case "hot": tours[0]=h;tours[1]=c; tours[2]=o;tours[3]=m;tours[4]=pr;break;
@@ -27,6 +39,16 @@ switch (weather)
   default: tours[1]="Sorry, I cannot understand.";
 }
 
+//location
+switch (location)
+{case "domestic" :tl[0]=nyc;tl[1]=h;tl[2]=o;tl[3]=m;tl[4]=lv;tl[5]=sf;tl[6]=la;tl[7]=mb;break;
+  case "hot": tours[0]=r;tours[1]=c; tours[2]=l;tours[3]=pr;tours[4]=p;break;
+  default: tours[1]="Sorry, I cannot understand.";
+}
+
+
+
+//test for weather
 /*for (int nn=0;nn<=12;nn++)
 {System.out.println(tours[nn]);*/
           System.out.print(tours[0]);
